bee8a8381f64ea4536e600360982658ba897dcf7
froemosen <kristianantonhedegaard@gmail.com>
2022-01-27 16:15:51 +0100

    works again

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Kristian\Desktop\Gym\Informatik\Flask
+set VIRTUAL_ENV=D:\Projekt-Webserver
 
 if not defined PROMPT set PROMPT=$P$G
 

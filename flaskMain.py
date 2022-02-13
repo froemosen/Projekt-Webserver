dcdcaf3ba77ee418be991c19733b994114a8f2bd
froemosen <kristianantonhedegaard@gmail.com>
2022-02-13 15:16:47 +0100

    Works now?

--- flaskMain.py
+++ flaskMain.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3 as sql
 from getData import GetData
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Example plot
+plt.plot([0], [0])
+plt.xlabel('Dates')
+plt.ylabel('YOUR CHOSEN DATA')
+plt.savefig('static/graph.png')
 
 app = Flask(__name__)
 
@@ -58,13 +66,50 @@ def list():
     return render_template("list.html",rows = rows)
 
 
-@app.route("/deathsPerInfected")
+@app.route("/deathsPerInfected", methods = ['POST', 'GET'])
 def deathsPerInfected():
     print("Getting deaths per infected...")
-    deaths = GetData.deaths()
-    infected = GetData.infected() 
+    #countries = GetData.countries()
+    countries = ("Danmark", "Tyskland", "Bosnien")
+    
+    #deaths = GetData.deaths()
+    #infected = GetData.infected() 
+    
     #y = 
-    return render_template("plotGraph.html", x=x, y=y)
+    return render_template("plotGraph.html", graphType = "deathsPerInfected", countries = countries)
+
+@app.route("/createGraph-deathsPerInfected", methods = ['POST', 'GET'])
+def plotGraph():
+    if request.method=="POST":
+        countries = request.form.getlist("country")
+        print(countries)
+        row = 0
+        fig, axs = plt.subplots(len(countries))
+        for country in countries:
+            #dates = GetData.dates(country)
+            #deaths = GetData.deaths(country)
+            #infected = GetData.infected(country)
+            dates = ["12/02-2020", "13/02-2020", "14/02-2020", "15/02-2020"]
+            deaths = [2, 5, 6, 8]
+            infected = [100, 615, 161, 1651]
+            values = [i / j for i, j in zip(deaths, infected)]
+            print(values)
+            
+            
+            axs[row].plot(dates, values)
+            axs[row].set_title(country)
+            #plt.subplot(len(countries), 1, row)
+            #plt.plot(dates, values)
+            #plt.title(country)
+            #plt.xlabel('Dates')
+            #plt.ylabel('Deaths per infected')
+            row += 1
+        fig.tight_layout()
+        plt.savefig('static/graph.png')
+        
+        return redirect(url_for('deathsPerInfected'))
+        
+                
 
 
 @app.route("/rawData")
